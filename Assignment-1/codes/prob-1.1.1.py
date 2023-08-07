bd10456ff715fdc6224fec2a6718d67ffa24cecc
ajaykrshna <ajaykfan@gmail.com>
2023-08-07 16:08:58 +0530

    changed variable names

--- Assignment-1/codes/prob-1.1.1.py
+++ Assignment-1/codes/prob-1.1.1.py
@@ -1,13 +1,13 @@
 import numpy as numpy
 
-vector_a = numpy.array([[1],[-1]])
-vector_b = numpy.array([[-4],[6]])
-vector_c = numpy.array([[-3],[-5]])
+A = numpy.array([[1],[-1]])
+B = numpy.array([[-4],[6]])
+C = numpy.array([[-3],[-5]])
 
-dir_ab = vector_b - vector_a
-dir_bc = vector_c - vector_b
-dir_ca = vector_a - vector_c
+AB = B - A
+BC = C - B
+CA = A - C
 
-print('direction vector of AB is \n', dir_ab)
-print('direction vector of BC is \n', dir_bc)
-print('direction vector of CA is \n', dir_ca)
+print('direction vector of AB is \n', AB)
+print('direction vector of BC is \n', BC)
+print('direction vector of CA is \n', CA)
