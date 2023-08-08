ac55d1ac6c91f6c4bb4158b8051c0ba03271a13d
ajaykrshna <ajaykfan@gmail.com>
2023-08-08 11:12:26 +0530

    added comments

--- Assignment-1/codes/prob-1.1.1.py
+++ Assignment-1/codes/prob-1.1.1.py
@@ -1,9 +1,17 @@
+# Written by
+# Ajay Krishnan K
+# EE22BTECH11003
+
+# Code for finding direction vectors
+
 import numpy as numpy
 
+# Create the vectors
 A = numpy.array([[1],[-1]])
 B = numpy.array([[-4],[6]])
 C = numpy.array([[-3],[-5]])
 
+# Find the direction vectors
 AB = B - A
 BC = C - B
 CA = A - C
