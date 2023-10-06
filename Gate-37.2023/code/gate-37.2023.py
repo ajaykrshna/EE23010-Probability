8640d66b3721a3d14dce3cf74afb6a20cef5a02e
ajaykrshna <ajaykfan@gmail.com>
2023-10-06 18:18:17 +0530

    updated code

--- Gate-37.2023/code/gate-37.2023.py
+++ Gate-37.2023/code/gate-37.2023.py
@@ -1,16 +1,20 @@
 import numpy as np
 
 # Define the number of random samples to generate
-num_samples = 100000
+num_samples = 1000
 
-# Generate random samples from the PDF of X using inverse transform sampling
-samples_x = np.random.uniform(1, np.exp(2), num_samples)  # Generate samples between 1 and e^2
-samples_y = np.log(samples_x)  # Calculate corresponding samples for Y
+# Generate random samples of X
+uniform_samples = np.random.uniform(0, 1, num_samples)
+samples_x = 1 / (1 - uniform_samples)
 
-# Calculate the conditional probability P(Y < 1 | Y < 2) by counting the samples that satisfy the condition
+# Calculate Y = ln(X) for the samples
+samples_y = np.log(samples_x)
+
+# Count the number of samples where Y < 1, Y < 2, and both conditions
 count_y_lt_1 = np.sum(samples_y < 1)
 count_y_lt_2 = np.sum(samples_y < 2)
 
+# Calculate the conditional probability P(Y < 1 | Y < 2)
 conditional_prob = count_y_lt_1 / count_y_lt_2
 
-print("Simulated Conditional Probability P(Y < 1 | Y < 2) =", conditional_prob)
+print("Conditional Probability P(Y < 1 | Y < 2) =", conditional_prob)
